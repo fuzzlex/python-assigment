621d44976a107bbaa64b482b1e56e14af136966f
emre <emrekt.2018@icloud.com>
2021-07-27 22:13:43 +0300

    Armstrong_number_updated

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -1,12 +1,16 @@
 i = 0
 plusagain = 0
-number =  input("Hackendiinzi? ")
+number =  input("Enter a number please? ")
+while not number.isdigit():
+  print("Your format is incorrect")
+  number = input("Enter a number please? ")
 a = list(number)
 while i < len(a) :
   x = int(a[i]) ** len(a) 
   plusagain = plusagain + x
+  
   i += 1
 if plusagain == int(number) :
   print(number, " is an Armstrong number")
 else:
-  print(number, " isn't an Armstrong number") 
+  print(number, " isn't an Armstrong number")
