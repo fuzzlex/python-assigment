4f578eb7de5ed38c54f11e7db8183908bc7dd681
emre <emrekt.2018@icloud.com>
2021-08-12 10:40:58 +0300

    FizzBuzz numbers updated

--- FizzBuzz_Number.py
+++ FizzBuzz_Number.py
@@ -1,15 +1,13 @@
-def number(a) :
-  number_list = list(range(100))
-  for a in number_list :
-   if a == 0 :
+number_list = list(range(100))
+for a in number_list :
+  if a == 0 :
     print("")
-   elif a in number_list[::15] :
-    print("Fizzbuzz" , sep="-")
-   elif a in number_list[::3] :
-    print("Fizz", sep="-")
-   elif a in number_list[::5] :
-    print("Buzz", sep="-")
-   elif a in number_list[::1] :
-    print(a, sep="-")
+  elif a in number_list[::15] :
+    print("Fizzbuzz")
+  elif a in number_list[::3] :
+    print("Fizz")
+  elif a in number_list[::5] :
+    print("Buzz")
+  elif a in number_list :
+    print(a)
   
-number(a)
